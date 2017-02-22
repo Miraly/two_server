ff70256bd6e273d9138b914c9b445db325f5948a
Ksenia D <Ksenia D>
2017-02-21 20:08:35 -0500

    creating two servers in node

---
+++ server.js
@@ -0,0 +1,33 @@
+// Here we require/import the HTTP module
+var http = require("http");
+
+// Here we define a port to listen to
+var PORTONE = 7000;
+var PORTTWO = 7500;
+
+// Here we create a generic function to handle requests and responses
+function handleRequestOne(request, response) {
+
+  // The below statement is triggered (client-side) when the user visits the PORT URL
+  response.end("Life is good");
+}
+
+function handleRequestTwo(request, response) {
+  response.end("Bad weather today!");
+}
+// Here we use the Node HTTP package to create our server.
+// We then pass it the handleRequest function to empower it with functionality.
+var serverOne = http.createServer(handleRequestOne);
+// Here we start our server so that it can begin listening to client requests.
+serverOne.listen(PORTONE, function() {
+  // The below statement is triggered (server-side) when a user visits the PORT URL
+  console.log("Server listening on: http://localhost:%s", PORTONE);
+});
+
+
+var serverTwo = http.createServer(handleRequestTwo);
+// Here we start our server so that it can begin listening to client requests.
+serverTwo.listen(PORTTWO, function() {
+  // The below statement is triggered (server-side) when a user visits the PORT URL
+  console.log("Server listening on: http://localhost:%s", PORTTWO);
+});
